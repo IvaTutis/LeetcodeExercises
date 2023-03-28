e7b3fa7a1ce7366be9f954581b9cd9d101c65910
Iva Tutiš <ivach14@gmail.com>
2023-03-28 16:58:10 +0200

    Time: 79 ms (57.33%), Space: 39.2 MB (90.19%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -7,15 +7,15 @@ public class Solution {
         var right=0; 
         var left=0;
         var maxLength=0;
-        var charset = new HashSet<char>();
+        var charSet = new HashSet<char>();
         
         while (right<s.Length){
-            if(!charset.Contains(s[right])){
-                charset.Add(s[right]);
+            if(!charSet.Contains(s[right])){
+                charSet.Add(s[right]);
                 right++;
-                maxLength=Math.Max(maxLength, charset.Count);
+                maxLength=Math.Max(maxLength, charSet.Count);
             }else{
-                charset.Remove(s[left]);
+                charSet.Remove(s[left]);
                 left++;
             }
         }
