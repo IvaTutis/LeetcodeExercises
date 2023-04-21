0833df533046e0074af567fbfa366ed45da8ed18
Iva Tutiš <ivach14@gmail.com>
2023-04-21 15:19:01 +0200

    Time: 102 ms (22.72%), Space: 40.5 MB (69.08%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            //cycle was found
+            if (slow == fast) { 
+                ListNode start = head;
+                while (start != slow) {
+                    start = start.next;
+                    slow = slow.next;
+                }
+                return start;
+            }
+        }
+        
+        //no cycle detected
+        return null; 
+    }
+}
